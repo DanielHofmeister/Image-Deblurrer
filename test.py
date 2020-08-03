23d5bfab010baaf06e025764e46a9bb946338382
DanielHofmeister <dhofmeister@wisc.edu>
2020-08-02 23:21:19 -0500

    Created class for window
    Setup class structure to handle events

--- test.py
+++ test.py
@@ -2,16 +2,32 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200, 200, 1000, 1000)
+        self.setWindowTitle("This is a window!")
+        self.initUI()
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("this is a label!")
+        self.label.move(50,50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("click button")
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("you pressed clicked")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
+
 def window():
   app = QApplication(sys.argv)
-  win = QMainWindow()
-  win.setGeometry(200, 200, 300, 300)
-  win.setWindowTitle("This is a window!")
-
-  label = QtWidgets.QLabel(win)
-  label.setText("this is a label!")
-  label.move(50,50)
-
+  win = MyWindow()
   win.show()
   sys.exit(app.exec_())
+
 window()
